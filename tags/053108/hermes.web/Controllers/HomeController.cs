c1b1de466cebb72a7ba217e71dc7b235d1f1111a
zpyoung <zpyoung@913ad398-4848-0410-8709-89f3f0763da4>
2008-06-01 04:20:47 +0000

    Copied remotely

---
+++ tags/053108/hermes.web/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using hermes.core.Domain.RssFeedAggregate;
+
+namespace hermes.web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IRssFeedRepository _feedRepo;
+        
+        public Action<string> RenderViewAction { get; set; }
+        public Action<string, object> RenderViewWithDataAction { get; set; }
+
+        public HomeController(IRssFeedRepository feedRepo)
+        {
+            _feedRepo = feedRepo;
+            //RenderViewAction = RenderView;  // RenderView(string)
+            //RenderViewWithDataAction = RenderView; // RenderView(string, object)
+        }
+        public ActionResult Index()
+        {
+            ViewData["Title"] = "Home Page";
+            ViewData["Message"] = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewData["Title"] = "About Page";
+
+            return View();
+        }
+
+        public void AddRssFeed(string feedUri)
+        {
+            var feed = new RssFeed { FeedUri = feedUri };
+            _feedRepo.SaveFeed(feed);
+        }
+    }
+}
---
+++ tags/053108/hermes/Views/Home/Index.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Views/Shared/Site.Master" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="hermes.Views.Home.Index" %>
+
+<asp:Content ID="indexContent" ContentPlaceHolderID="MainContentPlaceHolder" runat="server">
+    <p>
+    <form method="post" action='<%= Url.Action("AddRssFeed") %>'>
+   <input type="text" name="feedUri" />
+   <input type="submit" name="addTask" value="Add Feed" />
+</form>
+    </p>
+</asp:Content>
